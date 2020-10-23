c8dcf58056ad55fa1f0cf667c3b96d9d50eed46e
Darryl Kilzer <darrylkilzer@gmail.com>
2020-10-23 16:19:04 -0600

    init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link>|
+    <a href="https://othercornergames.github.io/blog">Blog</a>
   </div>
-  <router-view/>
+  <router-view />
 </template>
